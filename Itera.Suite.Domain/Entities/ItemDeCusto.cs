6e6b8089744c2c5a1122a20f754b073df8b251ef
Issao Toribio Takeuchi <89165394+issaotakeuchi@users.noreply.github.com>
2025-07-08 18:07:28 -0300

    Correct names of property

--- Itera.Suite.Domain/Entities/ItemDeCusto.cs
+++ Itera.Suite.Domain/Entities/ItemDeCusto.cs
@@ -1,12 +1,13 @@
 ﻿using Itera.Suite.Shared.Enums;
 using Itera.Suite.Domain.ValueObjects;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Itera.Suite.Domain.Entities;
 
 public class ItemDeCusto
 {
     public Guid Id { get; set; }
-    public Guid GrupoDeEstudoId { get; set; }
+    public Guid ProjetoDeViagemId { get; set; }
     public string Categoria { get; set; }         // Ex: "Transporte", "Hospedagem", "Guia"
     public string Descricao { get; set; }         // Ex: "Ônibus Transcomin", "Pousada Cavernas"
     public Guid? FornecedorId { get; set; }
@@ -14,6 +15,7 @@ public class ItemDeCusto
     public int Diarias { get; set; }              // Ex: 3
     public int Quantidade { get; set; }           // Ex: 24
     public decimal ValorUnitario { get; set; }    // Ex: 2.62
+    [NotMapped]
     public decimal Total => Diarias * Quantidade * ValorUnitario;
     public ICollection<ObservacaoItem> Observacoes { get; set; } = new List<ObservacaoItem>();
     public ICollection<PagamentoProgramado> Pagamentos { get; set; } = new List<PagamentoProgramado>();
