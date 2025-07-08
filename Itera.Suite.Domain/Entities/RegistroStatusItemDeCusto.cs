b2df64b6918fca8ffceb6a3af13166cf456cc6bf
Issao Toribio Takeuchi <89165394+issaotakeuchi@users.noreply.github.com>
2025-07-08 18:44:30 -0300

    Add new properties on RegistroStatusItemDeCusto

--- Itera.Suite.Domain/Entities/RegistroStatusItemDeCusto.cs
+++ Itera.Suite.Domain/Entities/RegistroStatusItemDeCusto.cs
@@ -5,8 +5,10 @@ namespace Itera.Suite.Domain.Entities;
 public class RegistroStatusItemDeCusto
 {
     public Guid Id { get; set; }
+    public Guid ItemDeCustoId { get; set; }
     public StatusItemDeCusto Status { get; set; }
     public DateTime DataHora { get; set; }
     public string UsuarioResponsavel { get; set; }
     public string? Justificativa { get; set; }
+    public string? ArquivoUrl { get; set; }
 }
